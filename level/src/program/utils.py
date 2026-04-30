74561e5992572947392b92ee043065122423dbec
Harley Wilson <dev@hjw.sh>
2026-04-30 09:18:11 +0800

    feat: add basic patch

--- level/src/program/utils.py
+++ level/src/program/utils.py
@@ -1,12 +1,5 @@
-import numpy as np
-# 248 * 86400 * 1000
-threshold = np.sum(np.array([2142720000], dtype=np.intc))
+THRESHOLD = 248 * 86400 * 1000
+
 
 def is_optimal(days):
-    # days * 86400 * 1000
-    a = np.array([days, 8640000], dtype=np.intc)
-    res = np.multiply(a[0], a[1])
-    if(res >= threshold):
-        return True
-    else:
-        return False
+    return days * 86400 * 1000 >= THRESHOLD
--- level/src/program/views.py
+++ level/src/program/views.py
@@ -1,14 +1,23 @@
-from django.http import HttpResponse, Http404
-from django.template import loader
+from django.http import HttpResponse, HttpResponseBadRequest
+
 from .utils import is_optimal
 
+
 def index(request):
-    return HttpResponse("[!] Connected to Boeing 787!<br>[?] Enter how many days this Boeing has been operational (1 to 248): http://localhost:8080/isoptimal?days=[1-248]: ")
+    return HttpResponse(
+        "[!] Connected to Boeing 787!<br>[?] Enter how many days this Boeing has been operational (1 to 248): http://localhost:8080/isoptimal?days=[1-248]: "
+    )
+
 
 def isoptimal(request):
-    days = int(request.GET["days"])
-    res = is_optimal(days)
-    if(res):
-        return HttpResponse(f"[i] Reboot is required")
+    try:
+        days = int(request.GET["days"])
+        if days <= 0:
+            raise ValueError
+        res = is_optimal(days)
+    except ValueError:
+        return HttpResponseBadRequest()
+    if res:
+        return HttpResponse("[i] Reboot is required")
     else:
         return HttpResponse(f"[i] System is optimal<br>Reboot is required in {248 - days} days")
